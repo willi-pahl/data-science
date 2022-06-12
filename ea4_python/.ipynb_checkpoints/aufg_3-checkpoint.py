87a9f4a3b75aab2b3bd931f24c9b16a2f4a61bdb
W. Pahl <willi.pahl@gmx.de>
2022-06-12 16:18:00 +0200

    add ea 7 excercise 2

---
+++ ea4_python/.ipynb_checkpoints/aufg_3-checkpoint.py
@@ -0,0 +1,4 @@
+import pandas as pd
+# df = pd.read_csv('https://github.com/edlich/eternalrepo/blob/master/DS-WAHLFACH/countries.csv')
+df = pd.read_csv('/home/pahl/Dokumente/Studium/Module/6_DS/github/ea4_python/countries.csv')
+print(df)
